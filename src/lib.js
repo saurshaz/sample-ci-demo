17e79316d7b51a4a58db021eb7c3bcf2dc9335bf
Saurabh S <saurabh@developer>
2019-04-29 16:39:38 +0530

    - build folder changes

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 
+// TODO: make this conditional, not in case of production NODE_ENV
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:3000/',
 });
